3c09ad882b8f1cb7f392053610ac8d9c5362c2dd
Nur Mohammad <nurmohammad6465@gmail.com>
2026-09-24 15:50:51 +0600

    Navbar Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
+import Navbar from "@/components/shared/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,9 +23,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
+      data-theme="dark"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Navbar/>
         {children}
             <ToastContainer/>
         </body>
---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+
+const Navbar = () => {
+    return (
+        <nav className="w-full border-b border-[#1A1C21] bg-[#0D0F12] text-white">
+            <div className="container mx-auto flex h-14 items-center justify-between px-4">
+                
+                <div className="flex items-center gap-2">
+                    <Image
+                        src={logo}
+                        alt="FITLOG logo"
+                        width={22}
+                        height={22}
+                        className="object-contain"
+                    />
+
+                    <h1 className="text-md font-bold tracking-wide">
+                        FITLOG
+                    </h1>
+                </div>
+
+                <div className="flex items-center gap-1 text-[14px]">
+                    <button className="rounded-full bg-[#263500] px-3 py-1.5 font-medium text-[#C2F800] cursor-pointer">
+                        Workouts
+                    </button>
+
+                    <button className="px-3 py-1.5 text-[#8B8D93] hover:text-white cursor-pointer">
+                        My Plan
+                    </button>
+                </div>
+
+                <div className="flex items-center gap-5 text-[10px]">
+                    
+                    <button className="flex items-center gap-2 text-[#B5B7BC] cursor-pointer">
+                        <span>Plan</span>
+                        <span className="flex h-3 w-3 items-center justify-center rounded-full bg-[#C2F800] text-[10px] font-bold text-black">
+                            0
+                        </span>
+                    </button>
+
+                    <button className="flex items-center gap-2 text-[#B5B7BC] cursor-pointer">
+                        <span>Saved</span>
+                        <span className="flex h-3 w-3 items-center justify-center rounded-full border border-[#44474F] text-[10px] text-[#8B8D93]">
+                            0
+                        </span>
+                    </button>
+
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
